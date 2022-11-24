ad2682a60192ca0cc1345e27bbdd9f12d653e965
Rafael Delgado <rafael@delgado.com>
2022-11-23 21:02:23 -0800

    Update to Library Project

--- CAP Tracker.Library/CAPTrackerData.cs
+++ CAP Tracker.Library/CAPTrackerData.cs
@@ -4,7 +4,7 @@ namespace CAP_Tracker.Library;
 public class CAPTrackerData
 {
 	public CAPTrackerData() {}
-	public CAPTrackerData(int capId, string nameLast, string nameFirst, DateOnly joinDate, string achvName, DateOnly? aprDate, DateOnly? nextApprovalDate)
+	public CAPTrackerData(int? capId, string nameLast, string nameFirst, DateOnly joinDate, string achvName, DateOnly? aprDate, DateOnly? nextApprovalDate)
 	{
 		CAPID = capId;
 		NameLast = nameLast;
@@ -14,62 +14,62 @@ public class CAPTrackerData
 		AprDate = aprDate;
 		NextApprovalDate = nextApprovalDate;
 	}
-	public int? CAPID { get; set; }
-	public string? NameLast { get; set; }
-	public string? NameFirst { get; set; }
-	public string? Email { get; set; }
-	public string? AchvName { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? AprDate { get; set; }
-	[JsonConverter(typeof(DateOnlyJsonConverter))]
-	public DateOnly JoinDate { get; set; }
-	public string? Region { get; set; }
-	public string? Wing { get; set; }
-	public string? Unit { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? PhyFitTest { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? LeadLabDateP { get; set; }
-	public int? LeadLabScore { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? AEDateP { get; set; }
-	public int? AEScore { get; set; }
-	public string? AEModuleOrTest { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? CharacterDevelopment { get; set; }
-	public bool? ActivePart { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? ActiveParticipationDate { get; set; }
-	public bool? CadetOath { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? CadetOathDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? LeadershipExpectationsDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? UniformDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? SpecialActivityDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? NextApprovalDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? DrillDate { get; set; }
-	public int? DrillScore { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? WelcomeCourseDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? EssayDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? SpeechDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? AEInteractiveDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? LeadershipInteractiveDate { get; set; }
-	public string? AEInteractiveModule { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? StaffServiceDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? OralPresentationDate { get; set; }
-	[JsonConverter(typeof(NullableDateOnlyJsonConverter))]
-	public DateOnly? TechnicalWritingAssignmentDate { get; set; }
-	public string? TechnicalWritingAssignment { get; set; }
+    public int? CAPID { get; set; }
+    public string? NameLast { get; set; }
+    public string? NameFirst { get; set; }
+    public string? Email { get; set; }
+    public string? AchvName { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? AprDate { get; set; }
+    [JsonConverter(typeof(DateOnlyJsonConverter))]
+    public DateOnly JoinDate { get; set; }
+    public string? Region { get; set; }
+    public string? Wing { get; set; }
+    public string? Unit { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? PhyFitTest { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? LeadLabDateP { get; set; }
+    public int? LeadLabScore { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? AEDateP { get; set; }
+    public int? AEScore { get; set; }
+    public string? AEModuleOrTest { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? CharacterDevelopment { get; set; }
+    public bool? ActivePart { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? ActiveParticipationDate { get; set; }
+    public bool? CadetOath { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? CadetOathDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? LeadershipExpectationsDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? UniformDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? SpecialActivityDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? NextApprovalDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? DrillDate { get; set; }
+    public int? DrillScore { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? WelcomeCourseDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? EssayDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? SpeechDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? AEInteractiveDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? LeadershipInteractiveDate { get; set; }
+    public string? AEInteractiveModule { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? StaffServiceDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? OralPresentationDate { get; set; }
+    [JsonConverter(typeof(NullableDateOnlyJsonConverter))]
+    public DateOnly? TechnicalWritingAssignmentDate { get; set; }
+    public string? TechnicalWritingAssignment { get; set; }
 }
--- CAP Tracker.Library/StringHelper.cs
+++ CAP Tracker.Library/StringHelper.cs
@@ -1,25 +1,25 @@
 namespace CAP_Tracker.Library;
 public static class ExtensionMethods
 {
-	public static DateOnly? ToDateOnly(this string value)
+	public static DateOnly? ToDateOnly(this string? value)
 	{
-		if (DateOnly.TryParse(value!, out var result))
+		if (value != null && DateOnly.TryParse(value!, out var result))
 		{
 			return new DateOnly?(result);
 		};
 		return new DateOnly?();
 	}
-	public static int? ToInt32(this string value)
+	public static int? ToInt32(this string? value)
 	{
-		if (Int32.TryParse(value, out var result))
+		if (value != null && Int32.TryParse(value!, out var result))
 		{
 			return new int?(result);
 		}
 		return new int?();
 	}
-	public static bool? ToBoolean(this string value)
+	public static bool? ToBoolean(this string? value)
 	{
-		if (Boolean.TryParse(value, out var result))
+		if (value != null && Boolean.TryParse(value!, out var result))
 		{
 			return new Boolean?(result);
 		};
