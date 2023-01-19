3ebca531b131db4c08301a47f6231e6481e3925f
Rafael Delgado <rafael@delgado.com>
2023-01-18 17:31:46 -0800

    add Cadet Overview

---
+++ CAP Tracker.App/Pages/CadetOverview.razor.cs
@@ -0,0 +1,24 @@
+﻿using CAP_Tracker.Library;
+using CAP_Tracker.Services;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using static System.Net.WebRequestMethods;
+
+namespace CAP_Tracker.App.Pages;
+
+public partial class CadetOverview
+{
+    public IEnumerable<Cadet>? Cadets { get; set; } = default;
+
+    [Inject]
+    protected ILogger<CadetOverview> Logger { get; set; } = default!;
+    [Inject]
+    protected HttpClient Http { get; set; } = default!;
+    protected async override Task OnInitializedAsync()
+    {
+        Cadets = await Http.GetFromJsonAsync<IEnumerable<Cadet>>("data/cadets.json");
+        Logger.LogDebug($"Cadets: {Cadets?.Count()}");
+    }
+}
