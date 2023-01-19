e6b2812a3550b04a4cdee31e9bd4323984b899d0
Rafael Delgado <rafael@delgado.com>
2023-01-18 17:30:52 -0800

    Add blazor app

---
+++ CAP Tracker.App/Program.cs
@@ -0,0 +1,12 @@
+using CAP_Tracker.App;
+using CAP_Tracker.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
