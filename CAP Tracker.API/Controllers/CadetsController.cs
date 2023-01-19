98c0a8a1f1b43888d48c3cca5c070514568010cc
Rafael Delgado <rafael@delgado.com>
2023-01-18 17:21:47 -0800

    add logging to the API project

--- CAP Tracker.API/Controllers/CadetsController.cs
+++ CAP Tracker.API/Controllers/CadetsController.cs
@@ -10,17 +10,27 @@ namespace CAP_Tracker.API.Controllers;
 public class CadetsController : ControllerBase
 {
     private readonly TrackerService trackerService;
-    public CadetsController(TrackerService tracker) => trackerService = tracker;
+    private readonly ILogger<CadetsController> logger;
+    public CadetsController(ILogger<CadetsController> logger, TrackerService tracker) 
+    {
+        this.logger = logger;
+        trackerService = tracker;
+    } 
 
     [HttpGet(Name = nameof(GetCadets))]
     [ProducesResponseType(typeof(IEnumerable<Cadet>), 200)]
-    public IActionResult GetCadets() => Ok(trackerService.Cadets);
+    public IActionResult GetCadets()
+    {
+        logger.LogDebug($"Called GetCadets\nCadets found: {trackerService.Cadets.Count()}");
+        return Ok(trackerService.Cadets);
+    }
 
     [HttpGet("{capId:int}", Name = nameof(GetCadet))]
     [ProducesResponseType(typeof(Cadet), 200)]
     public IActionResult GetCadet(int capId)
     {
         var cadet = trackerService.GetCadet(capId);
+        logger.LogDebug($"Called GetCadet\nCadet Lookup: {capId}");
         return cadet is null ? NotFound() : Ok(cadet);
     }
 }
